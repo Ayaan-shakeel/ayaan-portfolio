9787f3399c1c12a100941605b7ef1a121da7b3df
Hayyaanshakeel <ayyanshakeel899@gmail.com>
2026-04-02 11:19:04 -0700

    Portfolio begins

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AyaanPortfolio from './MyPortfolio'
+
+const App = () => {
+  return (
+    <div>
+      <AyaanPortfolio/>
+    </div>
+  )
+}
+
+export default App
